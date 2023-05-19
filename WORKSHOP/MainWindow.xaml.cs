6285e5c985ba39b171f9d79e5f31dc58ba803cb8
masajin <priscilla.effects@gmail.com>
2023-05-19 19:31:15 +0500

    Добавьте файлы проекта.

---
+++ WORKSHOP/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WORKSHOP
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Order(object sender, RoutedEventArgs e)
+        {
+            order_client order_client = new order_client();
+            order_client.Show();
+        }
+
+        private void Button_Supply(object sender, RoutedEventArgs e)
+        {
+            supplier_supply supplier_supply = new supplier_supply();
+            supplier_supply.Show();
+        }
+
+        private void Button_Product(object sender, RoutedEventArgs e)
+        {
+            product_category product_category = new product_category();
+            product_category.Show();
+        }
+    }
+}
