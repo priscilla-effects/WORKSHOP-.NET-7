db3489cc645e68c8c26132060b782b3d4bf40ac7
masajin <priscilla.effects@gmail.com>
2023-05-23 02:39:56 +0500

    v1.0.6

--- WORKSHOP/AddItem.xaml.cs
+++ WORKSHOP/AddItem.xaml.cs
@@ -35,14 +35,8 @@ namespace WORKSHOP
             e.Handled = regex.IsMatch(e.Text);
         }
 
-        private void Category(object sender, TextCompositionEventArgs e)
-        {
-            Regex regex = new Regex("[^1-2]+");
-            e.Handled = regex.IsMatch(e.Text);
-        }
-
         string conString = "Host=127.0.0.1;Port=5432;Database=postgres;Username=postgres;Password=1076;";
-        string sql = "INSERT INTO public.\"Product\" (p_brand, p_name, p_description, p_price, p_quantity, p_supplier, ct_id) VALUES (@value1, @value2, @value3, @value4, @value5, @value6, @value7)";
+        string sql = "INSERT INTO public.\"Product\" (p_brand, p_name, p_description, p_price, p_quantity, p_category) VALUES (@value1, @value2, @value3, @value4, @value5, @value6)";
         private void Button_Add(object sender, RoutedEventArgs e)
         {
             try
@@ -57,8 +51,7 @@ namespace WORKSHOP
                         cmd.Parameters.AddWithValue("value3", description.Text);
                         cmd.Parameters.AddWithValue("value4", Int32.Parse(price.Text));
                         cmd.Parameters.AddWithValue("value5", Int32.Parse(quantity.Text));
-                        cmd.Parameters.AddWithValue("value6", supplier.Text);
-                        cmd.Parameters.AddWithValue("value7", Int32.Parse(category.Text));
+                        cmd.Parameters.AddWithValue("value6", (category.Text));
                         cmd.ExecuteNonQuery();
                     }
                 }
@@ -71,9 +64,6 @@ namespace WORKSHOP
             catch (Exception)
             {
                 MessageBox.Show($"Ошибка добавления строки.");
-
-                AddItem AddItem = new();
-                AddItem.Show();
             }
         }
 
--- WORKSHOP/OrderClient.xaml.cs
+++ WORKSHOP/OrderClient.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,9 +18,31 @@ namespace WORKSHOP
 {
     public partial class OrderClient : Window
     {
+        private readonly DataSet ds = new();
+        private readonly DataTable dt = new();
+        string conString = "Host=127.0.0.1;Port=5432;Database=postgres;Username=postgres;Password=1076;";
         public OrderClient()
         {
             InitializeComponent();
+            var con = new NpgsqlConnection(conString);
+            con.Open();
+            string sql = ("SELECT * FROM public.\"Order_Product\"");
+            NpgsqlDataAdapter adapter = new(sql, con);
+            ds.Reset();
+            adapter.Fill(ds);
+            dt = ds.Tables[0];
+            Order_DG.ItemsSource = dt.DefaultView;
+            con.Close();
+        }
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Delete(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void Button_Close(object sender, RoutedEventArgs e)
